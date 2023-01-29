8efba73fa874d1ec42b47dd192fa8317b6ae18f9
unknown <ebinaliyas5@gmail.com>
2023-01-29 21:39:29 +0530

    priority add

--- src/storage.js
+++ src/storage.js
@@ -7,28 +7,33 @@ export const todoData = [
     title: 'Wash Clothes',
     date: '2023-01-31',
     text: 'Its the end of the month, Wash Your Cloths.',
+    priority: '10',
   },
   {
     title: 'Water Plants',
     date: '2025-01-01',
     text: 'Different people have different opinions on when and how to water the plants, but the most appropriate time of watering the plants is in the early morning or late evening..',
+    priority: '50',
   },
   {
     title: 'Sea side walk',
     date: '2023-02-06',
     text: 'Go for a Walk',
+    priority: '30',
   },
   {
     title: "Ebin's BirthDay",
     date: '2023-09-26',
     text: "Its Ebin's Birth Day, Remember to bring gift.",
+    priority: '90',
   },
 ];
 
-function Todo(title, date, text) {
+function Todo(title, date, text, priority) {
   this.title = title;
   this.date = date;
   this.text = text;
+  this.priority = priority;
 }
 
 export function closeForm() {
@@ -47,12 +52,12 @@ export function addNew() {
   const title = document.getElementById('todo').value;
   const data = document.getElementById('date').value;
   const text = document.getElementById('text').value;
+  const priority = document.getElementById('priority').value;
 
   if (title.length < 1 || date.length < 1) {
     return;
   } else {
-    const todo1 = new Todo(title, data, text);
-
+    const todo1 = new Todo(title, data, text, priority);
     todoData.push(todo1);
 
     resetForm();
@@ -61,6 +66,5 @@ export function addNew() {
     //prevent tasks from doubiling
     const page = (document.querySelector('.right-main').innerHTML = '');
     displayToPage();
-    console.log([...todoData])
   }
 }
--- src/todayElements.js
+++ src/todayElements.js
@@ -1,36 +1,62 @@
-
 import { todoData } from './storage';
 
 const main = document.querySelector('.right-main');
 
 export function displayToPage() {
+  console.log(todoData);
   todoData.forEach((x) => {
-    
     const box = document.createElement('div');
-    box.classList.add('box')
+    box.classList.add('box');
     main.appendChild(box);
 
-    const dateAndTitle = document.createElement('div')
-    dateAndTitle.classList.add('date-title-div')
-    box.appendChild(dateAndTitle)
+    const iconsDiv = document.createElement('div');
+    iconsDiv.classList.add('iconsDiv');
+    box.appendChild(iconsDiv);
 
-    for(let i in x){
-      if(i === "title"){
-        const titleText = document.createElement('h1') 
-        titleText.textContent = `${x[i]}`
-        dateAndTitle.appendChild(titleText)
-      }
-      else if(i === "date"){
-        const titleText = document.createElement('h3') 
-        titleText.textContent = `${x[i]}`
-        dateAndTitle.appendChild(titleText)
-      }
-      else if(i === "text"){
-        const titleText = document.createElement('p') 
-        titleText.textContent = `${x[i]}`
-        box.appendChild(titleText)
+    const titleDiv = document.createElement('div');
+    titleDiv.classList.add('titleDiv');
+    box.appendChild(titleDiv);
+
+    const dateDiv = document.createElement('div');
+    dateDiv.classList.add('dateDiv');
+    box.appendChild(dateDiv);
+
+    const noteDiv = document.createElement('div');
+    noteDiv.classList.add('noteDiv');
+    box.appendChild(noteDiv);
+
+    for (let i in x) {
+      console.log(x)
+      if (i === 'title') {
+        const titleText = document.createElement('h1');
+        titleText.textContent = `${x[i]}`;
+        titleDiv.appendChild(titleText);
+      } else if (i === 'date') {
+        const titleText = document.createElement('h3');
+        titleText.textContent = `${x[i]}`;
+        dateDiv.appendChild(titleText);
+      } else if (i === 'text') {
+        const titleText = document.createElement('p');
+        titleText.textContent = `${x[i]}`;
+        noteDiv.appendChild(titleText);
+      } else if (i === 'priority') {
+        let y = parseInt(x[i]);
+
+        const priorityx = document.createElement('h3');
+        if (y < 20) {
+          iconsDiv.classList.add('priority');
+          iconsDiv.classList.add('low');
+        } else if (y >= 20 && y < 50) {
+          iconsDiv.classList.add('priority');
+          iconsDiv.classList.add('medium');
+        } else if (y >= 50 && y < 80) {
+          iconsDiv.classList.add('priority');
+          iconsDiv.classList.add('high');
+        } else {
+          iconsDiv.classList.add('priority');
+          iconsDiv.classList.add('urgent');
+        }
       }
     }
   });
-
 }
